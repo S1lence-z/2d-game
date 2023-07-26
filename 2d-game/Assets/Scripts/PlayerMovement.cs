2b93c19cf1009e5ad4353ecdb64eed4a3bc4409d
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-26 01:24:04 -0400

    feat: updated scripts for camera and player movement

---
+++ 2d-game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerBody;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    // Variables
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        // the multiplication ensures joystick support as the value can vary
+        playerBody.velocity = new Vector2(dirX * moveSpeed, playerBody.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerBody.velocity = new Vector2(playerBody.velocity.x, jumpForce);
+        }
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        // animatior transition from idle to running and vice versa
+        if (dirX > 0f) // when running right
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f) // when running left
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
+}
