def281dbe86c1ba7b3c9158cf2b85f783b2b2cf0
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-27 01:26:06 -0400

    Closes #2: enabled word wrap in visual studio

--- 2d-game/Assets/Scripts/CameraController.cs
+++ 2d-game/Assets/Scripts/CameraController.cs
@@ -15,8 +15,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // it takes the new position of x and y but the z position remains the same
-        // simply put the camera does not rotate with the player
+        // it takes the new position of x and y but the z position remains the same simply put the camera does not rotate with the player
         transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
