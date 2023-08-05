57be74d48854c0e26afa7045841a7468f504b919
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-05 09:45:11 -0400

    Closes #14: added the finish line object

---
+++ 2d-game/Assets/Scripts/Finish.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private bool levelCompleted = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!levelCompleted && collision.gameObject.name == "Player")
+        {
+            print("You Completed This Level!");
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
+        }
+    }
+
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
