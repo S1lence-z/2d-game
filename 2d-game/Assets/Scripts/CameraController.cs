7380991a64b69167c41dfcbcf2a1b456b279d169
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-04 20:19:19 -0400

    fix: deleted the empty Start method

--- 2d-game/Assets/Scripts/CameraController.cs
+++ 2d-game/Assets/Scripts/CameraController.cs
@@ -6,13 +6,6 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     private void Update()
     {
         // it takes the new position of x and y but the z position remains the same simply put the camera does not rotate with the player
