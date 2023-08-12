20e670a71913c183478106b0a45639cb702e8bea
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-11 23:54:39 -0400

    fix: updated the FinishLevel class and script name

--- 2d-game/Assets/Scripts/FinishLevel.cs
+++ 2d-game/Assets/Scripts/FinishLevel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Finish : MonoBehaviour
+public class FinishLevel : MonoBehaviour
 {
     private bool levelCompleted = false;
 
