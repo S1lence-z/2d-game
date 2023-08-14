5ca9e8913aedcdd63869ba9d75390130533bfd62
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-15 01:32:19 +0200

    feat: added a script for multiple power up types

---
+++ 2d-game/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum Type
+    {
+        HealingOrb,
+        DoubleJump
+    }
+
+    public Type type;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Collect();
+        }
+    }
+
+    private void Collect()
+    {
+        switch(type)
+        {
+            case Type.HealingOrb:
+                PlayerLife.IncreaseHp();
+                break;
+
+            case Type.DoubleJump:
+                break;
+        }
+        Destroy(gameObject);
+    }
+}
