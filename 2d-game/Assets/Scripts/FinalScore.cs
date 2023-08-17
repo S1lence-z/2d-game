b710d018696528e579bc051ab002e4fe54294815
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-18 00:03:44 +0200

    fix: changed the class name to the script name

--- 2d-game/Assets/Scripts/FinalScore.cs
+++ 2d-game/Assets/Scripts/FinalScore.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FinalScores : MonoBehaviour
+public class FinalScore : MonoBehaviour
 {
     [SerializeField] private Text displayCurrentScore;
     [SerializeField] private Text displayHighScore;
