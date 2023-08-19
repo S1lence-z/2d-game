de8a6802ef6b167aacee69c0ec12a78f6b588d5c
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-19 12:42:14 +0200

    feat: added a player infinite life script

---
+++ 2d-game/Assets/Scripts/InfinitePlayerLife.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfinitePlayerLife : MonoBehaviour
+{
+    private GameObject player;
+    private Animator anim;
+    private Rigidbody2D rigidBody;
+    private BoxCollider2D boxCollider;
+    private float deathDelay = 2f;
+    private bool isDead = false;
+    private static int totalHp;
+    private Vector3 startPos;
+    [SerializeField] private Text displayHP;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        startPos = player.transform.position;
+        totalHp = 1;
+    }
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            rigidBody.velocity = Vector2.zero;
+            rigidBody.gravityScale = 0f;
+            if (boxCollider != null)
+            {
+                boxCollider.enabled = false;
+            }
+        }
+        displayHP.text = "HP: " + totalHp.ToString();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isDead && collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        DecreaseHp();
+        anim.SetTrigger("death");
+        if (totalHp <= 0)
+        {
+            StartCoroutine(Extensions.LoadSceneWithDelay("Game Over", deathDelay));
+            return;
+        }
+    }
+
+    private void DecreaseHp()
+    {
+        totalHp--;
+    }
+}
