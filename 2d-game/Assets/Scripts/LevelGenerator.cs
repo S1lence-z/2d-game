d2cafe22ec33cfdb5d661cbe45151bfa90f23585
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-19 23:54:30 +0200

    feat: added the level generator script

---
+++ 2d-game/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    private const float playerDistanceSpawnLevelPart = 200f;
+    [SerializeField] private Transform levelPartStart;
+    [SerializeField] private List<Transform> levelPartList;
+    [SerializeField] private GameObject player;
+    private Vector3 lastEndPosition;
+
+    private void Awake()
+    {
+        lastEndPosition = levelPartStart.Find("EndPosition").position;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(player.transform.position, lastEndPosition) < playerDistanceSpawnLevelPart)
+        {
+            SpawnLevelPart();
+        }
+    }
+
+    private void SpawnLevelPart()
+    {
+        Transform pickedLevelPart = levelPartList[Random.Range(0, levelPartList.Count)];
+        Transform lastLevelPart = GetLevelPart(pickedLevelPart, lastEndPosition);
+        lastEndPosition = lastLevelPart.Find("EndPosition").position;
+    }
+
+    private Transform GetLevelPart(Transform levelPart, Vector3 spawnPosition)
+    {
+        Transform LevelPart = Instantiate(levelPart, spawnPosition, Quaternion.identity);
+        return LevelPart;
+    }
+}
