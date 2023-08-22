7d4ea83d2c50ce49ab01e2d3b1d7f5b19307a997
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-22 19:30:23 +0200

    feat: renamed game over script to end game script

--- 2d-game/Assets/Scripts/EndGame.cs
+++ 2d-game/Assets/Scripts/EndGame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOver : MonoBehaviour
+public class EndGame : MonoBehaviour
 {
     public void GoToStartMenu()
     {
