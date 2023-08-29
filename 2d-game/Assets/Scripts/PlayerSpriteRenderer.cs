7372b3f3583884a51d1391b2dac112afdf333376
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-30 01:07:48 +0200

    feat: added animations to the dynamic player prefab

--- 2d-game/Assets/Scripts/PlayerSpriteRenderer.cs
+++ 2d-game/Assets/Scripts/PlayerSpriteRenderer.cs
@@ -7,47 +7,63 @@ public class PlayerSpriteRenderer : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     private PlayerMovement movement;
-
     [SerializeField] private AnimatedSprite idle;
     [SerializeField] private AnimatedSprite running;
     [SerializeField] private Sprite jump;
     [SerializeField] private Sprite fall;
     [SerializeField] private AnimatedSprite doubleJumping;
+    [SerializeField] private AnimatedSprite death;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = true;
         movement = GetComponentInParent<PlayerMovement>();
+    }
+
+    private void DisableAllMovement()
+    {
+        idle.enabled = false;
+        running.enabled = false;
+        doubleJumping.enabled = false;
     }
 
     private void LateUpdate()
     {
-        if (movement.state == PlayerMovement.PlayerMovementState.running)
+        if (!movement.enabled)
         {
-            running.enabled = true;
+            DisableAllMovement();
+            death.enabled = true;
         }
-        else { running.enabled = false; }
+        else
+        {
+            death.enabled = false;
+            if (movement.state == PlayerMovement.PlayerMovementState.doubleJumping)
+            {
+                doubleJumping.enabled = true;
+            }
+            else { doubleJumping.enabled = false; }
 
-        if (movement.state == PlayerMovement.PlayerMovementState.idle && !running.enabled)
-        {
-            idle.enabled = true;
-        }
-        else { idle.enabled = false; }
+            if (movement.state == PlayerMovement.PlayerMovementState.running)
+            {
+                running.enabled = true;
+            }
+            else { running.enabled = false; }
 
-        
-        if (movement.state == PlayerMovement.PlayerMovementState.doubleJumping)
-        {
-            doubleJumping.enabled = true;
-        }
-        else { doubleJumping.enabled = false; }
+            if (movement.state == PlayerMovement.PlayerMovementState.idle && !running.enabled)
+            {
+                idle.enabled = true;
+            }
+            else { idle.enabled = false; }
 
-        if (movement.state == PlayerMovement.PlayerMovementState.jumping)
-        {
-            spriteRenderer.sprite = jump;
-        }
-        else if (movement.state == PlayerMovement.PlayerMovementState.falling)
-        {
-            spriteRenderer.sprite = fall;
+            if (movement.state == PlayerMovement.PlayerMovementState.jumping)
+            {
+                spriteRenderer.sprite = jump;
+            }
+            else if (movement.state == PlayerMovement.PlayerMovementState.falling)
+            {
+                spriteRenderer.sprite = fall;
+            }
         }
     }
 }
