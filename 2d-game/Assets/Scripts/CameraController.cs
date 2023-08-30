e4b613437c61a6d18767c2543e6d01d05a33630c
Jiří Zelenka <j3.zelenka@gmail.com>
2023-08-30 15:53:58 +0200

    fix: changed update method to late update

--- 2d-game/Assets/Scripts/CameraController.cs
+++ 2d-game/Assets/Scripts/CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
 
-    private void Update()
+    private void LateUpdate()
     {
         // it takes the new position of x and y but the z position remains the same simply put the camera does not rotate with the player
         transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
