6273844d41abfeaf373d2ab8137060e2383bbb56
Jiří Zelenka <j3.zelenka@gmail.com>
2023-09-03 16:30:08 +0200

    feat: added comments to methods

--- space-runner/Assets/Scripts/AllTimeHighScore.cs
+++ space-runner/Assets/Scripts/AllTimeHighScore.cs
@@ -5,38 +5,53 @@ using UnityEngine.UI;
 
 public class AllTimeHighScore : MonoBehaviour
 {
-    [SerializeField] private Text levelsHighScore;
-    [SerializeField] private Text infiniteHighScore;
+    // SerializeField attribute makes private variables visible in the Unity Inspector.
+    [SerializeField] private Text levelsHighScore;   // Reference to a UI Text component for levels high score.
+    [SerializeField] private Text infiniteHighScore; // Reference to a UI Text component for infinite high score.
+
+    // Static variables to store the highest scores for levels and infinite modes.
     private static int levelsHS;
     private static int infiniteHS;
 
+    // This method is called when the object this script is attached to is created.
     private void Awake()
     {
-        levelsHS = GameScore.highScore;
-        infiniteHS = InfiniteGameScore.highScore;
+        // Initialize the static high score variables with the current high scores.
+        levelsHS = GameScore.highScore;           // Get the high score for levels mode.
+        infiniteHS = InfiniteGameScore.highScore; // Get the high score for infinite mode.
     }
 
+    // This method is called once per frame.
     private void Update()
     {
-        GetHighScores();
-        UpdateHighScores();
+        // Call two methods: GetHighScores() and UpdateHighScores().
+        GetHighScores();    // Update the high score variables if needed.
+        UpdateHighScores(); // Update the UI Text components with the high scores.
     }
 
+    // This method checks if the current game's high score surpasses the stored high score.
     private void GetHighScores()
     {
+        // Check if the current levels high score is greater than the stored high score.
         if (GameScore.highScore > levelsHS)
         {
-            levelsHS = GameScore.highScore;
+            levelsHS = GameScore.highScore; // Update the levels high score.
         }
+
+        // Check if the current infinite high score is greater than the stored high score.
         if (InfiniteGameScore.highScore > infiniteHS)
         {
-            infiniteHS = InfiniteGameScore.highScore;
+            infiniteHS = InfiniteGameScore.highScore; // Update the infinite high score.
         }
     }
 
+    // This method updates the UI Text components to display the current high scores.
     private void UpdateHighScores()
     {
+        // Update the text for the levels high score UI element.
         levelsHighScore.text = "Levels HS: " + levelsHS.ToString();
+
+        // Update the text for the infinite high score UI element.
         infiniteHighScore.text = "Infinite HS: " + infiniteHS.ToString();
     }
 }
--- space-runner/Assets/Scripts/AnimatedSprite.cs
+++ space-runner/Assets/Scripts/AnimatedSprite.cs
@@ -4,65 +4,71 @@ using UnityEngine;
 
 public class AnimatedSprite : MonoBehaviour
 {
-    public Sprite[] sprites;
-    public float framerate = 1f / 6f;
-    public bool playOneTime = false;
-    private SpriteRenderer spriteRenderer;
-    private int frame;
-    private bool isAnimating = false; // Added flag to control animation state
+    public Sprite[] sprites;        // An array to store the sprite frames for the animation.
+    public float framerate = 1f / 6f; // The frame rate at which the animation plays.
+    public bool playOneTime = false; // Flag to determine if the animation should play only once.
+    private SpriteRenderer spriteRenderer; // Reference to the SpriteRenderer component.
+    private int frame;               // The current frame of the animation.
+    private bool isAnimating = false; // Added flag to control animation state.
 
+    // This method is called when the script instance is initialized.
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component attached to this GameObject.
     }
 
+    // This method is called when the GameObject becomes active/enabled.
     private void OnEnable()
     {
         if (playOneTime)
         {
-            isAnimating = true;
-            StartCoroutine(AnimateOneTime());
+            isAnimating = true; // Set the animation flag to true for one-time play.
+            StartCoroutine(AnimateOneTime()); // Start the one-time animation coroutine.
         }
         else
         {
+            // Start repeating the animation using the specified frame rate.
             InvokeRepeating(nameof(Animate), framerate, framerate);
         }
     }
 
+    // This method is called when the GameObject becomes inactive/disabled.
     private void OnDisable()
     {
         if (playOneTime)
         {
-            isAnimating = false;
+            isAnimating = false; // Stop the one-time animation.
         }
         else
         {
-            CancelInvoke();
+            CancelInvoke(); // Cancel the repeating animation.
         }
     }
 
+    // Coroutine to play the animation once.
     private IEnumerator AnimateOneTime()
     {
         frame = 0;
         while (frame < sprites.Length && isAnimating)
         {
-            spriteRenderer.sprite = sprites[frame];
+            spriteRenderer.sprite = sprites[frame]; // Set the sprite to the current frame.
             frame++;
-            yield return new WaitForSeconds(framerate);
+            yield return new WaitForSeconds(framerate); // Wait for the specified frame rate.
         }
-        isAnimating = false;
+        isAnimating = false; // Animation is complete.
     }
 
+    // Method to handle the animation (repeating).
     private void Animate()
     {
         frame++;
         if (frame >= sprites.Length)
         {
-            frame = 0;
+            frame = 0; // Reset to the first frame when reaching the end of the sprites array.
         }
         if (frame >= 0 && frame < sprites.Length)
         {
-            spriteRenderer.sprite = sprites[frame];
+            spriteRenderer.sprite = sprites[frame]; // Set the sprite to the current frame.
         }
     }
 }
