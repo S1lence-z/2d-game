ece177a4a589963e158cd282c33d914ce10c344a
Jiří Zelenka <j3.zelenka@gmail.com>
2023-09-03 16:32:43 +0200

    feat: commented the code

--- space-runner/Assets/Scripts/EndGame.cs
+++ space-runner/Assets/Scripts/EndGame.cs
@@ -5,19 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
+    // This method is called when the "Go To Start Menu" button is clicked.
     public void GoToStartMenu()
     {
-        SceneManager.LoadScene("Start Screen");
-        ResetAllSettings();
+        SceneManager.LoadScene("Start Screen"); // Load the "Start Screen" scene.
+        ResetAllSettings(); // Reset various game settings and scores.
     }
 
+    // This method is called when the "Exit Game" button is clicked.
     public void ExitGame()
     {
-        Application.Quit();
+        Application.Quit(); // Quit the game application.
     }
 
+    // This method is called when the "Play Again" button is clicked.
     public void PlayAgain()
     {
+        // Check the chosen game mode and start the corresponding game mode.
         if (GameSettings.ChosenGameMode == GameSettings.GameMode.Levels)
         {
             StartGame.StartLevelsMode();
@@ -26,16 +30,18 @@ public class EndGame : MonoBehaviour
         {
             StartGame.StartInfiniteMode();
         }
-        ResetAllSettings();
+        ResetAllSettings(); // Reset various game settings and scores.
     }
+
+    // This private method is used to reset various game settings and scores.
     private void ResetAllSettings()
     {
         // Reset All Score Counters
-        GameScore.ResetScoreCount();
-        InfiniteGameScore.ResetScoreCount();
+        GameScore.ResetScoreCount(); // Reset the score counter for levels mode.
+        InfiniteGameScore.ResetScoreCount(); // Reset the score counter for infinite mode.
         // Reset Power Ups
-        PlayerMovement.DisableDoubleJump();
-        InfinitePlayerMovement.DisableDoubleJump();
-        PowerUpStatus.ResetDoubleJumpState();
+        PlayerMovement.DisableDoubleJump(); // Disable double jump power-up for levels mode.
+        InfinitePlayerMovement.DisableDoubleJump(); // Disable double jump power-up for infinite mode.
+        PowerUpStatus.ResetDoubleJumpState(); // Reset the double jump power-up state.
     }
 }
